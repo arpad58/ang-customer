3c1403e026c067bb94b3f09146efdb7d2530f12c
Arpad <solar@hu.inter.net>
2021-04-13 16:39:04 +0200

    table works

--- src/app/page/customer-list/customer-list.component.ts
+++ src/app/page/customer-list/customer-list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Customer } from 'src/app/model/customer';
+import { CustomerService } from 'src/app/service/customer.service';
 
 @Component({
   selector: 'app-customer-list',
@@ -7,7 +10,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CustomerListComponent implements OnInit {
 
-  constructor() { }
+  customerList$: Observable<Customer[]> = this.customerService.getAll();
+
+  constructor(
+    private customerService: CustomerService,
+  ) { }
 
   ngOnInit(): void {
   }
