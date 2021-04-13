0e9a8d01389d76fec11b501c15ecbc6cc28a404c
Arpad <solar@hu.inter.net>
2021-04-13 16:26:19 +0200

    getall

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './common/navbar/navbar.component';
+import { CustomerListComponent } from './page/customer-list/customer-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    CustomerListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/service/customer.service.ts
+++ src/app/service/customer.service.ts
@@ -1,9 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Customer } from '../model/customer';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CustomerService {
 
-  constructor() { }
+  serverAddress: string = 'https://nettuts.hu/jms/arpad58/customers';
+
+  constructor(private http: HttpClient) { }
+
+  getAll(): Observable<Customer[]> {
+    return this.http.get<Customer[]>(`${this.serverAddress}`);
+  }
+
 }
